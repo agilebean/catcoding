b6b75d83a6fa233e8736d82b070d2ba9cc6a2678
Chaehan So <chaehan.so@gmail.com>
2021-01-09 01:02:28 +0900

    added read_pci.R + _labels.R: data preprocessing of pci dataset (n=4418)

---
+++ _labels.R
@@ -0,0 +1,9 @@
+
+# filename <- "PCI-Translation"
+filename <- "PCI-Translation-cso"
+filename.xlsx <- paste0(data.prefix, filename, ".xlsx") 
+filename.rds <- paste0(data.prefix, filename, ".rds") %>% tolower
+
+dataset.label <- "data/dataset hexaco.rds"
+data.prefix <- "data/pci/"
+
