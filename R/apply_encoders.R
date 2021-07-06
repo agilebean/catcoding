833c6a94ec16d37cdaae97a30f688f4d3ab4f6f5
Chaehan So <chaehan.so@gmail.com>
2021-07-06 15:50:59 +0900

    moved apply_encoder.R to apply_encoders.R

--- R/apply_encoders.R
+++ R/apply_encoders.R
@@ -1,3 +1,57 @@
+################################################################################
+# apply encoding on dataset
+apply_encoder <- function(data_prepped, encoding) {
+  
+  print("######################################################################")
+  print(paste("ENCODING:", encoding))
+  print("#####################")
+  print("Encoding...")
+  
+  # data_prepped <- data.prepped
+  training.original <- data_prepped$training.set
+  testing.original <- data_prepped$testing.set
+  target.label <- data_prepped$target.label
+  
+  encoding_function <- case_when(
+    encoding == "vtreat-cross" ~ "apply_vtreat_cross",
+    encoding == "vtreat-design" ~ "apply_vtreat_design",
+    encoding == "vtreat-dummy" ~ "apply_vtreat_dummy",
+    startsWith(encoding, "embed") ~ "apply_embed_encoder",
+    startsWith(encoding, "scikit") ~ "apply_scikit_encoder",
+    encoding == "integer-encoding" ~ "apply_integer_encoder",
+    encoding == "factor-encoding" ~ "apply_factor_encoder"
+  ) %>% get()
+  
+  # apply encoding function
+  time.encoding <- system.time(
+    data.encoded <- encoding_function(
+      encoding, training.original, testing.original, target.label
+    )
+  ) %>% .["elapsed"] %>% round(., digits = 3)
+  
+  # get categorical features
+  no.cats <- training.original %>% select(where(is.factor)) %>% ncol
+  
+  # get #features-original (-1 for target)
+  no.features.original <- training.original %>% ncol -1
+  
+  # get #features-encoded (-1 for target)
+  no.features.encoded <- data.encoded$training.set %>% ncol -1
+  
+  # inform about feature generation stats
+  print("#####################")
+  print(paste("...finished encoding in:", time.encoding, "seconds"))
+  print(paste(
+    "From", no.cats, "categorical of", no.features.original,
+    "original features in total, generated", 
+    no.features.encoded, "features."
+  ))
+  print("######################################################################")
+  
+  return(data.encoded)
+  
+}
+
 # apply ALL encoders on 1 split object
 apply_encoders_dataset <- function(data_prepped, encoder_list) {
   
@@ -13,7 +67,7 @@ apply_encoders_data_list <- function(data_label_list, encoder_list, save = TRUE)
            apply_encoders_dataset(., encoder_list) %>% 
            {
              if (save) {
-               saveRDS(file = dataset_filename(dataset_label = .x))
+               saveRDS(object = ., file = dataset_filename(dataset_label = .x))
              } else {
                .
              }
