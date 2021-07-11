63e0e397b4927b1ee8ae42dcb771a806b07b0b92
Chaehan So <chaehan.so@gmail.com>
2021-07-11 23:30:08 +0900

    added data-raw for swbsun dataset from Sun2018 paper

---
+++ data-raw/swbsun.R
@@ -0,0 +1,26 @@
+## code to prepare `swbsun` dataset goes here
+filepath <- system.file(
+  "extdata",
+  "AspectsWB_2_Sample2.xlsx",
+  package = "catcoding"
+) %>% print
+
+dataset <- readxl::read_excel(filepath)
+
+dataset %<>% 
+  rowwise() %>% 
+  mutate(SWB = mean(c_across(starts_with("SWLS")))) %>% 
+  select(starts_with("BFAS"), SWB) 
+
+# define target and features
+target.label <- "SWB"
+features.labels <- dataset %>% select(-target.label) %>% names
+
+# create data structure
+swbsun <- list()
+swbsun$target.label <- target.label
+swbsun$features.labels <- features.labels
+swbsun$data <- dataset
+
+usethis::use_data(swbsun, overwrite = TRUE)
+
